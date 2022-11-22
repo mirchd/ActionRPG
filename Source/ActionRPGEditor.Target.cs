0d249e2454c3b4b88edb82eae42bbffc5d6c5a0a
DianChen <mirchd@hotmail.com>
2022-11-22 11:27:46 +0800

    build order lastest“

--- Source/ActionRPGEditor.Target.cs
+++ Source/ActionRPGEditor.Target.cs
@@ -8,6 +8,7 @@ public class ActionRPGEditorTarget : TargetRules
 	public ActionRPGEditorTarget( TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Editor;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Latest;
 		DefaultBuildSettings = BuildSettingsVersion.V2;
 		ExtraModuleNames.AddRange( new string[] { "ActionRPG" } );
 	}

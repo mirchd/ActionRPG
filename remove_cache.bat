4582c261cf8dd1ef1dcaab47fbb33d2177409913
DianChen <mirchd@hotmail.com>
2022-12-07 20:21:50 +0800

    delete script

--- remove_cache.bat
+++ remove_cache.bat
@@ -8,17 +8,11 @@ rd /s/q %~dp0PersistentDownloadDir
 rd /s/q %~dp0Debug
 rd /s/q %~dp0DerivedDataCache
 rd /s/q %~dp0Saved
-rd /s/q %~dp0Plugins\HotPatcher\Binaries
-rd /s/q %~dp0Plugins\HotPatcher\Intermediate
-rd /s/q %~dp0Plugins\UnLua\Binaries
-rd /s/q %~dp0Plugins\UnLua\Intermediate
-rd /s/q %~dp0Plugins\UnLuaExtensions\Binaries
-rd /s/q %~dp0Plugins\UnLuaExtensions\Intermediate
-rd /s/q %~dp0Plugins\UnLuaTestSuite\Binaries
-rd /s/q %~dp0Plugins\UnLuaTestSuite\Intermediate
 del %~dp0*.sln
 
 FOR /F "tokens=*" %%G IN ('DIR /B /AD /S bin') DO RMDIR /S /Q "%%G"
 FOR /F "tokens=*" %%G IN ('DIR /B /AD /S obj') DO RMDIR /S /Q "%%G"
+FOR /F "tokens=*" %%G IN ('DIR /B /AD /S Binaries') DO RMDIR /S /Q "%%G"
+FOR /F "tokens=*" %%G IN ('DIR /B /AD /S Intermediate') DO RMDIR /S /Q "%%G"
 
 pause

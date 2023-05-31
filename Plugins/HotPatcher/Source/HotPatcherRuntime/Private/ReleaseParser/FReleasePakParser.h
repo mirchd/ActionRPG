679951c3504df2c7fb02637721dc59a9de5a5148
DianChen <mirchd@hotmail.com>
2023-05-31 14:49:42 +0800

    delete hotpacher

--- Plugins/HotPatcher/Source/HotPatcherRuntime/Private/ReleaseParser/FReleasePakParser.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include "IReleaseParser.h"
-
-struct FReleasePakParser : public IReleaseParser
-{
-	virtual void Parser(TSharedPtr<FReleaseParserConf> ParserConf, EHashCalculator HashCalculator) override;
-};

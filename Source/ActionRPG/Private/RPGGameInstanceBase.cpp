e98576105185851572e6e311ac339286b1e23c35
DianChen <mirchd@hotmail.com>
2024-03-07 14:21:37 +0800

    chunkdownloader

--- Source/ActionRPG/Private/RPGGameInstanceBase.cpp
+++ Source/ActionRPG/Private/RPGGameInstanceBase.cpp
@@ -1,6 +1,12 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 
 #include "RPGGameInstanceBase.h"
+
+/** 访问ChunkDownloader，以及一些用于管理资产和委托的有用工具 */
+#include "ChunkDownloader.h"
+#include "Misc/CoreDelegates.h"
+#include "AssetRegistryModule.h"
+
 #include "RPGAssetManager.h"
 #include "RPGSaveGame.h"
 #include "Items/RPGItem.h"
@@ -10,6 +16,133 @@ URPGGameInstanceBase::URPGGameInstanceBase()
 	: SaveSlot(TEXT("SaveGame"))
 	, SaveUserIndex(0)
 {}
+
+void URPGGameInstanceBase::Init()
+{
+	Super::Init();
+
+	/** 执行以下步骤可确保ChunkDownloader已初始化，准备开始下载内容，并告知其他函数清单的状态。 */
+
+	// https://docs.unrealengine.com/5.3/en-US/hosting-a-manifest-and-assets-for-chunkdownloader-in-unreal-engine/
+	/** 测试固定值 */
+	// DefaultGame.ini
+	// [/Script/Plugins.ChunkDownloader PatchingLive] +CdnBaseUrls=127.0.0.1/PatchingCDN
+	/** 正式版本 */
+	// 使用HTTP请求获取 ContentBuildID。该函数使用这些变量中的信息来向你的网站请求清单。
+	const FString DeploymentName = "PatchingLive";
+	const FString ContentBuildId = "PatchingKey";
+
+	// 用选定平台初始化文件块下载器
+	TSharedRef<FChunkDownloader> Downloader = FChunkDownloader::GetOrCreate();
+	// 所需平台名称 TargetDownloadsInFlight赋予值 8 ，该值设置ChunkDownloader同时处理的最大下载数量。
+	Downloader->Initialize("Windows", 8);
+
+	// 加载缓存的版本ID 检查磁盘上是否已经下载文件，如果它们是最新清单文件，则ChunkDownloader可以跳过下载流程。
+	Downloader->LoadCachedBuild(DeploymentName);
+
+	// 更新版本清单文件
+	// 输出操作成功或失败的回调
+	TFunction<void(bool bSuccess)> UpdateCompleteCallback = [&](bool bSuccess) {
+		bIsDownloadManifestUpToDate = true;
+	};
+	// 下载清单文件的更新版本
+	Downloader->UpdateBuild(DeploymentName, ContentBuildId, UpdateCompleteCallback);
+}
+
+void URPGGameInstanceBase::Shutdown()
+{
+	Super::Shutdown();
+
+	// 关闭ChunkDownloader 停止当前正在进行的所有ChunkDownloader下载，然后清理并卸载该模块
+	FChunkDownloader::Shutdown();
+}
+
+void URPGGameInstanceBase::GetLoadingProgress(int32& BytesDownloaded, int32& TotalBytesToDownload, float& DownloadPercent, int32& ChunksMounted, int32& TotalChunksToMount, float& MountPercent) const
+{
+	// 获取ChunkDownloader的引用
+	TSharedRef<FChunkDownloader> Downloader = FChunkDownloader::GetChecked();
+
+	// 获取加载统计结构体
+	FChunkDownloader::FStats LoadingStats = Downloader->GetLoadingStats();
+
+	// 获取已下载和要下载的的字节
+	BytesDownloaded = LoadingStats.BytesDownloaded;
+	TotalBytesToDownload = LoadingStats.TotalBytesToDownload;
+
+	// 获取已挂载文件块数和要下载的文件块数
+	ChunksMounted = LoadingStats.ChunksMounted;
+	TotalChunksToMount = LoadingStats.TotalChunksToMount;
+
+	// 使用以上统计信息计算下载和挂载百分比
+	DownloadPercent = ((float)BytesDownloaded / (float)TotalBytesToDownload) * 100.0f;
+	MountPercent = ((float)ChunksMounted / (float)TotalChunksToMount) * 100.0f;
+}
+
+bool URPGGameInstanceBase::PatchGame()
+{
+	// 确保下载清单是最新的
+	// 检查清单是否是当前最新的。如果你尚未初始化ChunkDownloader并成功获取清单的新副本，则 bIsDownloadManifestUpToDate 将为false，并且此函数将返回false，表示无法开始补丁。
+	if (bIsDownloadManifestUpToDate)
+	{
+		// 获取文件块下载器 引用
+		TSharedRef<FChunkDownloader> Downloader = FChunkDownloader::GetChecked();
+
+		// 报告当前文件块状态
+		// 遍历下载列表并检查每个文件块的状态。
+		for (int32 ChunkID : ChunkDownloadList)
+		{
+			int32 ChunkStatus = static_cast<int32>(Downloader->GetChunkStatus(ChunkID));
+			UE_LOG(LogTemp, Display, TEXT("Chunk %i status: %i"), ChunkID, ChunkStatus);
+		}
+
+		TFunction<void(bool bSuccess)> DownloadCompleteCallback = [&](bool bSuccess) {
+			// 当每个单独的文件块完成下载时，将调用 当每个文件块成功下载或下载失败时，它将输出一条消息。
+			OnDownloadComplete(bSuccess);
+		};
+		// 开始下载所需文件块，这些文件块在 ChunkDownloadList 中列出。在调用此函数之前，必须用你想要的文件块ID填充此列表
+		Downloader->DownloadChunks(ChunkDownloadList, DownloadCompleteCallback, 1);
+
+		// 启动加载模式
+		TFunction<void(bool bSuccess)> LoadingModeCompleteCallback = [&](bool bSuccess) {
+			// 所有文件块下载完毕后，就会触发
+			OnLoadingModeComplete(bSuccess);
+		};
+		// 加载模式将告知ChunkDownloader开始监视其下载状态。
+		// 可以在不调用加载模式的情况下在后台被动下载文件块，使用它将输出下载统计信息，使你可以创建一个可以跟踪用户下载进度的UI。
+		// 下载整批文件块时，你还可以使用该回调函数运行特定功能。
+		Downloader->BeginLoadingMode(LoadingModeCompleteCallback);
+		
+		return true;
+	}
+
+	// 我们无法联系服务器验证清单，因此你无法修补
+	UE_LOG(LogTemp, Display, TEXT("Manifest Update Failed. Can't patch the game"));
+
+	return false;
+}
+
+// 清单更新完成后，此函数将用作异步回调。
+void URPGGameInstanceBase::OnManifestUpdateComplete(bool bSuccess)
+{
+	// GameInstance可以全局识别此补丁阶段已完成
+	bIsDownloadManifestUpToDate = bSuccess;
+}
+
+void URPGGameInstanceBase::OnDownloadComplete(bool bSuccess)
+{
+}
+
+void URPGGameInstanceBase::OnLoadingModeComplete(bool bSuccess)
+{
+	// 将传递给 OnDownloadComplete ，后者将在后续步骤中继续挂载文件块。
+	OnDownloadComplete(bSuccess);
+}
+
+void URPGGameInstanceBase::OnMountComplete(bool bSuccess)
+{
+	// 将指示所有文件块均已完成挂载，并且内容可用。
+	OnPatchComplete.Broadcast(bSuccess);
+}
 
 void URPGGameInstanceBase::AddDefaultInventory(URPGSaveGame* SaveGame, bool bRemoveExtra)
 {
--- Source/ActionRPG/Public/RPGGameInstanceBase.h
+++ Source/ActionRPG/Public/RPGGameInstanceBase.h
@@ -9,6 +9,9 @@
 class URPGItem;
 class URPGSaveGame;
 
+// 动态组播委托：该委托输出一个布尔值，该布尔值将告知你补丁下载操作是否成功。委托通常用于响应异步操作，例如下载或安装文件。
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FPatchCompleteDelegate, bool, Succeeded);
+
 // 它是 GameInstance 的游戏特定子类，是大多数游戏所必需的。由于在整个游戏中只声明一个游戏实例，它很适合存储全局Gameplay数据。
 /**
  * Base class for GameInstance, should be blueprinted
@@ -24,28 +27,19 @@ public:
 	// Constructor
 	URPGGameInstanceBase();
 
-	/** List of inventory items to add to new players */
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Inventory)
-	TMap<FPrimaryAssetId, FRPGItemData> DefaultInventory;
+	/** virtual function to allow custom GameInstances an opportunity to set up what it needs */
+	virtual void Init() override;
 
-	/** Number of slots for each type of item */
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Inventory)
-	TMap<FPrimaryAssetType, int32> ItemSlotsPerType;
+	/** virtual function to allow custom GameInstances an opportunity to do cleanup when shutting down */
+	virtual void Shutdown() override;
 
-	/** The slot name used for saving */
-	UPROPERTY(BlueprintReadWrite, Category = Save)
-	FString SaveSlot;
+	UFUNCTION(BlueprintPure, Category="Patching|Stats")
+	void GetLoadingProgress(int32& BytesDownloaded, int32& TotalBytesToDownload, float& DownloadPercent, int32& ChunksMounted, int32& TotalChunksToMount, float& MountPercent) const;
 
-	/** The platform-specific user index */
-	UPROPERTY(BlueprintReadWrite, Category = Save)
-	int32 SaveUserIndex;
-
-	/** Delegate called when the save game has been loaded/reset */
-	UPROPERTY(BlueprintAssignable, Category = Inventory)
-	FOnSaveGameLoaded OnSaveGameLoaded;
-
-	/** Native delegate for save game load/reset */
-	FOnSaveGameLoadedNative OnSaveGameLoadedNative;
+	// 启动游戏补丁过程。如果补丁清单不是最新的，则返回false
+	// 此函数提供了蓝图的一种公开的补丁过程启动方式。它返回布尔值指示成功还是失败。这是下载管理和其他类型异步任务中的通用模式。
+	UFUNCTION(BlueprintCallable, Category = "Patching")
+	bool PatchGame();
 
 	/**
 	 * Adds the default inventory to the inventory array
@@ -87,7 +81,43 @@ public:
 	UFUNCTION(BlueprintCallable, Category = Save)
 	void ResetSaveGame();
 
+public:
+
+	// 委托
+	// 补丁过程成功或失败时触发 提供了一个在补丁操作完成后与蓝图挂接的位置
+	UPROPERTY(BlueprintAssignable, Category = "Patching")
+	FPatchCompleteDelegate OnPatchComplete;
+
+	/** List of inventory items to add to new players */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Inventory)
+	TMap<FPrimaryAssetId, FRPGItemData> DefaultInventory;
+
+	/** Number of slots for each type of item */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Inventory)
+	TMap<FPrimaryAssetType, int32> ItemSlotsPerType;
+
+	/** The slot name used for saving */
+	UPROPERTY(BlueprintReadWrite, Category = Save)
+	FString SaveSlot;
+
+	/** The platform-specific user index */
+	UPROPERTY(BlueprintReadWrite, Category = Save)
+	int32 SaveUserIndex;
+
+	/** Delegate called when the save game has been loaded/reset */
+	UPROPERTY(BlueprintAssignable, Category = Inventory)
+	FOnSaveGameLoaded OnSaveGameLoaded;
+
+	/** Native delegate for save game load/reset */
+	FOnSaveGameLoadedNative OnSaveGameLoadedNative;
+
 protected:
+
+	// 要尝试和下载的文件块ID列表
+	// 将使用此列表保存后续要下载的所有文件块ID。在开发设置中，可以根据需要使用资产列表对此进行初始化。但是出于测试目的，只需公开默认值，以使用 蓝图编辑器 填写它们。
+	UPROPERTY(EditDefaultsOnly, Category="Patching")
+	TArray<int32> ChunkDownloadList;
+
 	/** The current save game object */
 	UPROPERTY()
 	URPGSaveGame* CurrentSaveGame;
@@ -104,6 +134,23 @@ protected:
 	UPROPERTY()
 	bool bPendingSaveRequested;
 
+	/** 用我们网站上托管的清单文件追踪本地清单文件是否为最新文件。*/
+	bool bIsDownloadManifestUpToDate;
+
+
+protected:
 	/** Called when the async save happens */
 	virtual void HandleAsyncSave(const FString& SlotName, const int32 UserIndex, bool bSuccess);
+
+	/** 在文件块下载进程完成时调用 */
+	void OnManifestUpdateComplete(bool bSuccess);
+
+	// 在文件块下载进程完成时调用
+	void OnDownloadComplete(bool bSuccess);
+
+	// ChunkDownloader加载模式完成时调用
+	void OnLoadingModeComplete(bool bSuccess);
+
+	// ChunkDownloader完成挂载文件块时调用
+	void OnMountComplete(bool bSuccess);
 };
